f7533194ce1ddceba9bf5d382560c4459d72439d
samuelsavioo <samueelsavio@gmail.com>
2026-02-19 16:03:38 -0300

    release 1.0.0

---
+++ OneDrive/Documentos/Programação/SamFinance/index.js
@@ -0,0 +1,158 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const port = 3000;
+
+
+app.use(express.json());
+
+
+app.get('/users', async (req, res) => {
+    try {
+     const [rows] = await db.query('SELECT * FROM users');
+     res.json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro ao conectar no bando de dados: ' + err.message);
+    }
+    
+});
+
+
+app.get('/categories', async (req, res) => {
+    try {
+     const [rows] = await db.query('SELECT * FROM categories');
+     res.json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro ao conectar no bando de dados: ' + err.message);
+    }
+    
+});
+
+app.get('/transactions', async (req, res) => {
+    try { `const sql = 
+     SELECT
+      t.*,
+      c.name AS category_name 
+      FROM transactions t
+      INNER JOIN categories c ON t.category_id = c.id;`
+     const [rows] = await db.query('SELECT * FROM transactions');
+     res.json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro ao conectar no bando de dados: ' + err.message);
+    }
+    
+});
+
+app.get('/', (req, res) => {
+    res.send('SamFinance Online e pronto para o banco!')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+
+app.post('/users', async (req, res) => {
+    const { name, email, password} = req.body;
+
+    try {
+        const query = 'INSERT INTO users ( name, email, password) VALUES (?, ?, ?)';
+        const [result] = await db.query(query, [ name, email, password]);
+
+        res.status(201).json({
+            message: 'Usuário cadastrado com sucesso!',
+            id: result.insertId
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao cadastrar usuário!')
+    }
+});
+
+app.post('/categories', async (req, res) => {
+    const {users_id, name, type} = req.body;
+
+    try {
+        const query = 'INSERT INTO categories (users_id, name, type) VALUES (?, ?, ?)';
+        const [result] = await db.query(query, [users_id, name, type]);
+
+        res.status(201).json({
+            message: 'Categoria cadastrada com sucesso!',
+            id: result.insertId
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao cadastrar categoria!')
+    }
+});
+
+app.post('/transactions', async(req, res) => {
+    const {users_id, category_id, description, amount, date} = req.body;
+
+    try {
+        const query = 'INSERT INTO transactions (users_id, category_id, description, amount, date) VALUES (?, ?, ?, ?, ?)';
+        const [result] = await db.query(query, [users_id, category_id, description, amount, date ]);
+
+        res.status(201).json({
+            message: 'Transação realizada!',
+            id: result.insertId
+        }); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Transação não realizada, tente novamente mais tarde')
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const query = 'DELETE FROM users WHERE id = ?';
+        const [result] = await db.query(query, [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).send('Usuário não encontrado');
+        }
+        res.json({message: 'Usuário deletado com sucesso'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao deletar usuário');
+    }
+});
+
+app.delete('/categories/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const query = 'DELETE FROM categories WHERE id = ?';
+        const [result] = await db.query(query, [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).send('Categoria não encontrada');
+        }
+        res.json({message: 'Categoria deletado com sucesso'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao deletar categoria');
+    }
+});
+
+app.delete('/transactions/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const query = 'DELETE FROM transactions WHERE id = ?';
+        const [result] = await db.query(query, [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).send('Transação não encontrada');
+        }
+        res.json({message: 'Transação deletado com sucesso'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao deletar transação');
+    }
+});
